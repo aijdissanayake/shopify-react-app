b315d10911db6a37f92af20462ecd08344171d4f
dammakaru <dammakaru@gmail.com>
2018-01-19 12:07:35 +0530

    removed code which was commented out from collapse2.js

--- src/components/collapse2.js
+++ src/components/collapse2.js
@@ -7,14 +7,9 @@ import { isUndefined } from 'util';
 class Collapse2 extends Component {
     constructor(props) {
         super(props);
-        // this.toggleCollapse = this.toggleCollapse.bind(this);
         this.fulfillOrder = this.fulfillOrder.bind(this);
         this.state = { collapsed: true };
     }
-
-    // toggleCollapse() {
-    //     this.setState({ collapsed: !this.state.collapsed });
-    // }
 
     fulfillOrder(){
        const url = '/shopify/shop-api/orders/' + this.props.orderID + '/fulfill';
@@ -26,7 +21,6 @@ class Collapse2 extends Component {
         console.log("collapse products");
         console.log(this.props.products);
         console.log(typeof this.props.products);
-        // let resourceThumbnails = [];
         let resourceList = this.props.itemArray.map((resItem, index) => {
             let productImage = "no/image";
             if (!isUndefined(this.props.products.length) && !isUndefined(this.props.products)) {
@@ -36,17 +30,6 @@ class Collapse2 extends Component {
 
                 if (!isUndefined(product[0])) {
                     productImage = product[0].images[0].src;
-                    // if(resourceThumbnails.length < 5){
-                    //     resourceThumbnails.push(                        
-                    //         <Thumbnail key={resourceThumbnails.length}
-                    //         source={product[0].images[0].src}
-                    //         alt={" Image"}
-                    //     />
-                    //     );
-                    // }
-                    // else if (resourceThumbnails.length == 5) {
-                    //     resourceThumbnails.push(<p key={resourceThumbnails.length}><b>. . .</b></p>);                            
-                    // }
                 }
             }
 
@@ -70,15 +53,9 @@ class Collapse2 extends Component {
             <div>
                 
                 <Container>
-                {/* <Collapse isOpen={this.state.collapsed}>
-                <Stack alignment="baseline" wrap={false}> {resourceThumbnails} </Stack>
-                </Collapse> */}
                     <Row>
                         <Col sm="7">
                             <Row style={{paddingBottom:50}}>
-                                {/* <Col sm="4">
-                                    <Button primary onClick={this.toggleCollapse} >{this.state.collapsed ? " Show Order Items " : " Hide Order Items "}</Button>
-                                </Col> */}
                                 <Col sm="4">
                                     <Button primary onClick={this.fulfillOrder}>Mark as Fulfilled</Button>
                                 </Col>
@@ -86,15 +63,12 @@ class Collapse2 extends Component {
                         </Col>
                     </Row>
                 </Container>
-                {/* <Collapse isOpen={!this.state.collapsed}> */}
                     <ResourceList
                         items={resourceList}
                         renderItem={(item, index) => {
                             return <ResourceList.Item key={index} {...item} />;
                         }}
                     />
-
-                {/* </Collapse> */}
             </div>
         );
     }
